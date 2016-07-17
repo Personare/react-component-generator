a9deadd6f907125920116b90525693aba237465e
Cauê Alves <cauealveseng@gmail.com>
2016-07-16 23:05:55 -0300

    Some changes
    1. Adding type of the prompt questions.
    2. Use promise for get the answers.
    3. Renamed the folder when starts with `_` to `.`

--- slushfile.js
+++ slushfile.js
@@ -4,46 +4,51 @@ const gulp = require('gulp');
 const install = require('gulp-install');
 const rename = require('gulp-rename');
 const template = require('gulp-template');
+const conflict = require('gulp-conflict');
 const inquirer = require('inquirer');
 const _ = require('underscore.string');
 
 gulp.task('default', done => {
-    let prompts = [
+    inquirer.prompt([
         {
+            type: 'input',
             name: 'componentName',
             message: 'What is the name of component?'
         },
         {
+            type: 'input',
             name: 'componentVersion',
             message: 'What is the version?',
             default: '0.1.0'
         }
-    ];
-
-    inquirer.prompt(prompts, answers => {
-        let files = [
-            __dirname + '/templates/**'
-        ];
-
+    ]).then(answers => {
         if (!answers.componentName) {
-
             return done();
         }
+
+        let files = [
+            __dirname + '/templates/**/*'
+        ];
 
         answers.componentName = _.humanize(answers.componentName);
         answers.componentSlugName = _.slugify(answers.componentName);
 
-        gulp.src(files)
+        return gulp.src(files)
             .pipe(template(answers))
             .pipe(rename(file => {
+                console.log(file);
                 if (file.basename[0] === '_') {
                     file.basename = '.' + file.basename.slice(1);
+                } else if (file.dirname[0] === '_') {
+                    file.dirname = '.' + file.dirname.slice(1);
                 }
             }))
-            .pipe(conflict('./'))
+            .pipe(conflict('./', {
+                defaultChoice: 'd'
+            }))
             .pipe(gulp.dest('./'))
             .pipe(install())
-            .on('end', () => {
+            .on('finish', function(){
                 done();
             });
     });
