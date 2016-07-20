ae3110292df6c3a472e594e6065cf6c17385128b
Cauê Alves <cauealveseng@gmail.com>
2016-07-19 22:01:45 -0300

    Fix indentation size

--- templates/tests/mock-browser.js
+++ templates/tests/mock-browser.js
@@ -5,12 +5,12 @@ global.document = jsdom('');
 global.window = document.defaultView;
 
 Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
 });
 
 global.navigator = {
-  userAgent: 'node.js'
+    userAgent: 'node.js'
 };
