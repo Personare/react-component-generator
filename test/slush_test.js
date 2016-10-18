b41ef1efaf906a8a58e2b6fc232ab55e7dbf3665
Cauê Alves <ceasbz2@gmail.com>
2016-10-18 19:11:00 -0200

    Improve the tests

--- test/slush_test.js
+++ test/slush_test.js
@@ -65,5 +65,63 @@ describe('react-component-generator', () => {
       })
     })
 
+    describe('should be created the default files', () => {
+      beforeEach(() => {
+        mockirer(inquirer, {
+          name: 'slush-test',
+          description: 'Unit tests',
+          version: '1.0.0',
+          ci: 'scrutinizer',
+          confirm: true
+        })
+      })
+
+      it('should be created the .github/ files', done => {
+        gulp.start('default').once('stop', () => {
+          mockGulpDest.assertDestContains([
+            '.github/ISSUE_TEMPLATE.md',
+            '.github/PULL_REQUEST_TEMPLATE.md'
+          ])
+          done()
+        })
+      })
+
+      it('should be created the storybook/ files', done => {
+        gulp.start('default').once('stop', () => {
+          mockGulpDest.assertDestContains([
+            'storybook/config.js',
+            'storybook/webpack.config.js',
+            'storybook/webpack.dist.config.js'
+          ])
+          done()
+        })
+      })
+
+      it('should be created the src/ files with the correct name', done => {
+        gulp.start('default').once('stop', () => {
+          mockGulpDest.assertDestContains([
+            'src/SlushTest.js',
+            'src/SlushTest.css',
+            'src/SlushTest.story.js',
+            'src/SlushTest.test.js'
+          ])
+          done()
+        })
+      })
+
+      it('should be created the root path files', done => {
+        gulp.start('default').once('stop', () => {
+          mockGulpDest.assertDestContains([
+            '.babelrc',
+            '.editorconfig',
+            '.gitignore',
+            'contributing.md',
+            'package.json',
+            'README.md'
+          ])
+          done()
+        })
+      })
+    })
   })
 })
