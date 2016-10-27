ae8a0c07580ec908c5898cd09eb70fa1bbbf6593
Cauê Alves <ceasbz2@gmail.com>
2016-10-27 18:49:22 -0200

    fix: standard are broken

--- templates/src/MyComponent.js
+++ templates/src/MyComponent.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 
 import './<%= camelName %>.css'
 
-function <%= camelName %>(props) {
+function <%= camelName %> (props) {
   return <div>Hello {props.name}</div>
 }
 
